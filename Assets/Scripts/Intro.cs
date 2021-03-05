4f9ae1e02bfd5f518f4f870d33051cffc50173c9
ThisIsMeWinning <daltoncclinton@gmail.com>
2021-03-04 22:57:52 -0800

    Created UI

---
+++ Assets/Scripts/Intro.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intro : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject intro;
+
+    public void Start()
+    {
+        //For random song
+        //int rand = Random.Range(1, 4);
+        //FindObjectOfType<AudioManager>().Play("Menu" + rand.ToString());
+        FindObjectOfType<AudioManager>().Play("Menu1");
+    }
+    void Update()
+    {
+        if(Input.anyKey)
+        {
+            FindObjectOfType<AudioManager>().Play("Click");
+            intro.SetActive(false);
+            mainMenu.SetActive(true);
+        }
+        
+    }
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    public void Start()
+    {
+
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+
+        for (int i =0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+
+        
+    }
+
+    public void SetResolution(int resolutionIndex) 
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void OpenURL()
+    {
+        Application.OpenURL("https://github.com/ThisIsMeWinning/aa-replica");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("QUIT");
+    }
+
+    public void PlayClick()
+    {
+        FindObjectOfType<AudioManager>().Play("Click");
+    }
+
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullScreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
